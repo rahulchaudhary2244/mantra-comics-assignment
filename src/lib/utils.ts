9e3266920a62b4310a423b961727325b6b55a468
Rahul Chaudhary <rahulchaudhary2244@gmail.com>
2024-10-14 00:56:18 +0530

    removing unused code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,10 +6,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const fetchData = async <T>(url: string) => {
-  const response = await fetch(url, {
-    headers: new Headers({
-      'Content-Type': 'application/json',
-    }),
-  });
+  const response = await fetch(url);
   return (await response.json()) as T;
 };
